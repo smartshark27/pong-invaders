23c5ef11706a856fb40bdfa524a475244e6230ce
Thomas Smart <thomassmart@Thomass-MacBook-Pro.local>
2025-03-29 16:02:13 +1100

    Make player movement smooth

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -5,13 +5,12 @@ signal game_over
 const SPEED = 1000.0
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	velocity.x = 0
 
 	if get_parent().running and Input.is_action_pressed("mouse_button_left"):
-		# Move player towards mouse on X axis
-		var diff = get_global_mouse_position() - position
-		var target = diff.normalized().x
-		velocity.x = target * SPEED
+		# Move player smoothly towards mouse on X axis
+		var diff = get_global_mouse_position().x - position.x
+		velocity.x = diff * SPEED * delta
 
 	move_and_slide()
